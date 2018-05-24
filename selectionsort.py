11514fedb77cc706b75c8882a077ccc1cf774575
UddeepaWijerathna <wijerathnauddeepa@gmail.com>
2018-05-24 17:32:38 +0530

    selectionsort

---
+++ selectionsort.py
@@ -0,0 +1,14 @@
+def selectionSort(lst):
+    for j in range(0,len(lst)):
+        max=0
+        for i in range (1,len(lst)-j):
+            if lst[i]>lst[max]:
+                max=i
+            lst[max],lst[len(lst)-1-j]=lst[len(lst)-1-j],lst[max]
+
+
+    return lst
+print(selectionSort([3 , 6 ,23 ,1 ,89,45, 67]))
+            
+            
+    
